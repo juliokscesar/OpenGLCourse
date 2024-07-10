73c42f13f5a29a3d7f900d59067816fa7f46d7ae
Julio Cesar <jcmcs177@gmail.com>
2024-07-09 22:26:41 -0300

    defined copy operator for static mesh

--- StaticMesh.hpp
+++ StaticMesh.hpp
@@ -15,6 +15,19 @@ public:
 
     StaticMesh(const StaticMesh& other);
 
+    StaticMesh& operator=(const StaticMesh& other)
+    {
+        if (this != &other)
+        {
+            this->m_VBO = other.m_VBO;
+            this->m_VAO = other.m_VAO;
+            this->m_vertices = other.m_vertices;
+            this->m_useIndexedDrawing = other.m_useIndexedDrawing;
+        }
+
+        return *this;
+    }
+
     void SetVertexAttribute(GLuint loc, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void* offsetSize) const noexcept;
     void SetEnabledVertexAttribute(GLuint loc, bool enable) const noexcept;
 
